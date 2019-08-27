ec0439ea992c17ac04920d1b00188dc713e960b2
Susmita Sarkar <sarkarsusmita253@gmail.com>
2019-08-27 18:11:23 +0530

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,10 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    Hello There!
+    <ion-app>
+      <ion-vue-route></ion-vue-route>
+    </ion-app>
   </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
-</script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
